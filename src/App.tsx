d83b5b9ad0c8d0444df74699d6bae6f588b7371a
0xFuneral <90068290+0xFuneral@users.noreply.github.com>
2022-04-26 05:33:27 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,7 @@ function App() {
             <section className="info">
 
                 <h1>3,333 Unique Pop Skullys</h1>
-                <h4>Pop Skullys are a collection of 3,333 randomly generated NFTs collectibles made from hundreds of traits designed by Funeral. The Cult is real. We are here for the art. We are here for the tech.</h4>
+                <h4>Pop Skullys are a collection of 3,333 randomly generated collectibles made from hundreds of traits designed by Funeral. The Cult is real. We are here for the art. We are here for the tech.</h4>
                 
             </section>
             <section className="team">
